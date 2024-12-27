c8eb9d1d1bed7e1276d144fe09a456ce86c23011
Anusha <147178514+A2003nu@users.noreply.github.com>
2024-12-27 22:43:40 +0530

    Add files via upload

---
+++ Basic/Perfect.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Perfect{
+    
+static boolean perfect(int num){
+    int sum=0;
+    for(int i=1;i<num;i++){
+        if(num%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum==num){
+        return true;
+    }
+    return false;
+}
+    public static void main(String[] args) {
+        int n=6;
+        if(perfect(n)){
+        System.out.println("It is perfect number");
+    }
+    else{
+         System.out.println("It is not perfect number");
+    }
+}
+}
